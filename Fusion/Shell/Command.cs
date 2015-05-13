a13a13cb8280cadeab4b1c4090a58b62a9a28052
demiurghg <demiurghg@gmail.com>
2015-05-13 21:26:06 +0300

    Shell stuff

---
+++ Fusion/Shell/Command.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion.Shell {
+	public abstract class Command {
+		public abstract void Execute ();
+		public abstract void Rollback ();
+	}
+}
---
+++ Fusion/Shell/Shell.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion.Shell {
+	public class Shell {
+
+		/// <summary>
+		/// Executes given string.
+		/// </summary>
+		/// <param name="command"></param>
+		public void ExecuteString ( string command )
+		{
+			throw new NotImplementedException();
+		}
+		
+
+		/// <summary>
+		/// Gains access to object properties.
+		/// Example:
+		///		RegisterNamedObject( "Config:Rendering/Lighting", RenderSystem.Lighting.Config );
+		///		RegisterNamedObject( "System:Rendering/Lighting", RenderSystem.Lighting );
+		///		RegisterNamedObject( "System:Rendering/Lighting", RenderSystem.Lighting );
+		///		
+		///			Config:Rendering/Lighting/SkipShadows
+		///			
+		///			set Content:Textures/Walls/Wall01.tga Compression BC1
+		/// </summary>
+		/// <param name="domain"></param>
+		/// <param name="name"></param>
+		/// <param name="obj"></param>
+		public void RegisterNamedObject ( string path, object obj )
+		{
+			throw new NotImplementedException();
+		}
+
+
+		/// <summary>
+		/// Ungains access to object properties.
+		/// </summary>
+		/// <param name="domain"></param>
+		/// <param name="name"></param>
+		/// <param name="obj"></param>
+		public void UnregisterNamedObject ( object obj )
+		{
+			throw new NotImplementedException();
+		}
+
+	}
+}
