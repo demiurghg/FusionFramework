9e2fd1695d504873a724c61f16ca4848576f2159
demiurghg <demiurghg@gmail.com>
2015-10-17 15:55:37 +0300

    TestGame2 — new playground

---
+++ Samples/TestGame2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Fusion;
+using Fusion.Build;
+
+namespace TestGame2 {
+	class Program {
+		[STAThread]
+		static void Main ( string[] args )
+		{
+			Trace.Listeners.Add( new ColoredTraceListener() );
+
+			try {
+				Builder.Build( @"..\..\..\Content", @"Content", @"..\..\..\Temp", false );
+			} catch ( Exception e ) {
+				Log.Error( e.Message );
+				return;
+			}
+
+			using (var game = new TestGame2()) {
+				game.Run( args );
+			}
+		}
+	}
+}
---
+++ Samples/TestGame2/TestGame2.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion;
+using Fusion.Core.Mathematics;
+using Fusion.Drivers.Graphics;
+using Fusion.Drivers.Input;
+using Fusion.Core.Content;
+using Fusion.Core.Development;
+using Fusion.Engine.Common;
+
+namespace TestGame2 {
+	public class TestGame2 : Game {
+		/// <summary>
+		/// TestGame2 constructor
+		/// </summary>
+		public TestGame2 ()
+			: base()
+		{
+			//	enable object tracking :
+			Parameters.TrackObjects = true;
+
+			//	uncomment to enable debug graphics device:
+			//	(MS Platform SDK must be installed)
+			//	Parameters.UseDebugDevice	=	true;
+
+			//	add services :
+			AddService( new SpriteBatch( this ), false, false, 0, 0 );
+			AddService( new DebugStrings( this ), true, true, 9999, 9999 );
+			AddService( new DebugRender( this ), true, true, 9998, 9998 );
+
+			//	add here additional services :
+
+			//	load configuration for each service :
+			LoadConfiguration();
+
+			//	make configuration saved on exit :
+			Exiting += Game_Exiting;
+		}
+
+
+		/// <summary>
+		/// Initializes game :
+		/// </summary>
+		protected override void Initialize ()
+		{
+			//	initialize services :
+			base.Initialize();
+
+			//	add keyboard handler :
+			InputDevice.KeyDown += InputDevice_KeyDown;
+
+			//	load content & create graphics and audio resources here:
+		}
+
+
+
+		/// <summary>
+		/// Disposes game
+		/// </summary>
+		/// <param name="disposing"></param>
+		protected override void Dispose ( bool disposing )
+		{
+			if (disposing) {
+				//	dispose disposable stuff here
+				//	Do NOT dispose objects loaded using ContentManager.
+			}
+			base.Dispose( disposing );
+		}
+
+
+
+		/// <summary>
+		/// Handle keys
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void InputDevice_KeyDown ( object sender, InputDevice.KeyEventArgs e )
+		{
+			if (e.Key == Keys.F1) {
+//				DevCon.Show( this );
+			}
+
+			if (e.Key == Keys.F5) {
+				Reload();
+			}
+
+			if (e.Key == Keys.F12) {
+				GraphicsDevice.Screenshot();
+			}
+
+			if (e.Key == Keys.Escape) {
+				Exit();
+			}
+		}
+
+
+
+		/// <summary>
+		/// Saves configuration on exit.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		void Game_Exiting ( object sender, EventArgs e )
+		{
+			SaveConfiguration();
+		}
+
+
+
+		/// <summary>
+		/// Updates game
+		/// </summary>
+		/// <param name="gameTime"></param>
+		protected override void Update ( GameTime gameTime )
+		{
+			var ds	=	GetService<DebugStrings>();
+
+			ds.Add( Color.Orange, "FPS {0}", gameTime.Fps );
+			ds.Add( "F1   - show developer console" );
+			ds.Add( "F5   - build content and reload textures" );
+			ds.Add( "F12  - make screenshot" );
+			ds.Add( "ESC  - exit" );
+
+			base.Update( gameTime );
+
+			//	Update stuff here :
+		}
+
+
+
+		/// <summary>
+		/// Draws game
+		/// </summary>
+		/// <param name="gameTime"></param>
+		/// <param name="stereoEye"></param>
+		protected override void Draw ( GameTime gameTime, StereoEye stereoEye )
+		{
+			base.Draw( gameTime, stereoEye );
+
+			//	Draw stuff here :
+		}
+	}
+}
