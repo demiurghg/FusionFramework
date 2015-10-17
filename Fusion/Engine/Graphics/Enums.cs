f318efd89e0fd0ce5fcadc461e7306511cb443d5
demiurghg <demiurghg@gmail.com>
2015-10-17 19:57:53 +0300

    Some graphics engine stuff

---
+++ Fusion/Engine/Graphics/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fusion.Engine.Graphics {
+
+
+	public enum VertexFormat {
+		Sprite,
+		LitRigid,
+		ShadowRigid,
+		LitSkinned,
+		ShadowSkinned,
+	}
+
+
+	public enum FrameFormat {
+		Ldr,
+		Hdr,
+	}
+}
---
+++ Fusion/Engine/Graphics/GraphicsEngine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fusion.Engine.Common;
+
+namespace Fusion.Engine.Graphics {
+
+	class GraphicsEngine {
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="engine"></param>
+		public GraphicsEngine ( GameEngine engine )
+		{
+		}
+
+
+		/// <summary>
+		/// Renders current scene to given target and camera
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="camera"></param>
+		public void RenderScene ( RenderFrame target, Camera camera )
+		{
+		}
+
+
+		/// <summary>
+		/// Renders all sprites to specified target
+		/// </summary>
+		/// <param name="target"></param>
+		public void RenderSprites (	RenderFrame target )
+		{
+		}
+
+
+		/// <summary>
+		/// Clears all instances, sprites and lights.
+		/// </summary>
+		public void Clear ()
+		{
+		}
+
+
+		/// <summary>
+		/// Mesh instances
+		/// </summary>
+		ICollection<MeshInstance> Instances { get; set; }
+
+
+		/// <summary>
+		/// Sprites 
+		/// </summary>
+		ICollection<Sprite> Sprites { get; set; }
+
+
+		/// <summary>
+		/// Omni-lights
+		/// </summary>
+		ICollection<OmniLight> OmniLights { get; set; }
+
+
+		/// <summary>
+		/// Spotlights
+		/// </summary>
+		ICollection<SpotLight> SpotLights { get; set; }
+	}
+}
